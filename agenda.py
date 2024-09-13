71c6841d516e87427c75bf6fd612ff7b775e604b
Mateus Machado Costa <mateusdoug@hotmail.com>
2024-09-13 11:27:31 -0300

    classe contato criada e agenda atualizada

--- agenda.py
+++ agenda.py
@@ -1,5 +1,11 @@
+import Pyro4
+
+
+@Pyro4.expose
+@Pyro4.behavior(instance_mode="single")
 class Agenda:
-    def __init__(self):
+    def __init__(self, nomeAgenda, ipNS, ipSV):
+        self.iniciar(nomeAgenda, ipNS, ipSV)
         self.estaOnline = True
         self.contatos = []
 
@@ -16,3 +22,13 @@ class Agenda:
         for contato in self.contatos:
             if contato.nome == nomeContato:
                 return contato
+
+    def iniciar(self, nomeAgenda, ipNS, ipSV):
+        daemon = Pyro4.Daemon(host=ipSV)
+        try:
+            ns = Pyro4.locateNS(host=ipNS, port=9090)
+            uri = daemon.register(self)
+            ns.register(nomeAgenda, uri)
+            daemon.requestLoop()
+        except Exception as e:
+            print(e)
---
+++ contato.py
@@ -0,0 +1,10 @@
+class Contato:
+    def __init__(self, nome, telefone):
+        self.nome = nome
+        self.telefone = telefone
+
+    def alterarNome(self, nomeNovo):
+        self.nome = nomeNovo
+
+    def alterarTelefone(self, telefoneNovo):
+        self.telefone = telefoneNovo
